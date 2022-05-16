74f3e8404aa16a630bfd95d98b0afdbaf40ce643
Timothy Money <timothypmoney@gmail.com>
2022-05-16 10:59:23 -0500

    port revision

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const express = require('express')
 const app = express()
 
 // Declare a port number here
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('./'));
 
